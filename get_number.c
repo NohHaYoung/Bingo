80522cca297997ef430fe8f678081de73ebc8b3f
NohHaYoung <42965065+NohHaYoung@users.noreply.github.com>
2018-11-13 21:07:24 +0900

    bingo
    get_numberby : do-while문 수정

--- get_number.c
+++ get_number.c
@@ -1,41 +1,67 @@
 #include <stdio.h>
 #include <time.h>
 
-int process_bingo(int A, int B[][A], int C[][A])
+/*입력받은 값과 빙고테이블의 값이 일치할 경우 해당 테이블의 값을 -1로 바꿈*/
 
-int get_number_byMe(int A, int B, int C[][A], int D[][A]){
+int process_bingo(int N, int Num, int Table1[][N], int Table2[][N]){
 	
-	int max=A*A;
+	int i, j;
+	
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			if(Table1[i][j]==Num){
+				Table1[i][j] = -1;
+				Table2[i][j] = -1;
+			} 
+		}
+	}
+	
+}
+
+int get_number_byMe(int N, int Num, int Table1[][N], int Table2[][N]){
+	
+	int max=N*N;
 	int i, j;
 	
 	/*사용자가 원하는 정수를 입력 받음*/
-	 printf("숫자(1~%d)를 입력하시오:\n", max);
-	 scanf("%d", &B);
+	 printf("숫자(1~%d)를 입력하시오 : ", max);
+	 scanf("%d", &Num);
 
 	
 	/*사용자가 입력한 정수 처리
 	1. 입력한 정수와 배열이 일치할 경우 process_bingo로 처리
 	2. 입력한 정수가 빙고판에 없는 숫자일 경우 다시 입력받음*/
-	do{for(i=0;i<A;i++){
-		for(j=0;j<A;j++){
-			if(B!=C[i][j]){
+	do{for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			if(Num==Table1[i][j]){
+				break;
+			}
+			else{
 				printf("ERROR! 다른 숫자를 입력하시오.");
-				scanf("%d",&B);
+				scanf("%d",&Num);
 			}
 		 }
 		}
-	}while(B==C[i][j]){
-	 break;
+	}while(Num==Table1[i][j]);{
+	 process_bingo(N, Num, Table1, Table2);
 	}
 }
 
-int get_number_byCom(int A, int B, int C[][A], int D[][A]){
+int get_number_byCom(int N, int Num, int Table1[][N], int Table2[][N]){
 
-	int max=A*A;
+	int max=N*N;
+	int i, j;
 	
 	/*컴퓨터가 입력할 정수, 범위 내의 난수 발생 후 하나 선택*/
 	
-	do{	srand((unsigned int)time(NULL)); 
-		B = rand()%max+1
-	} while(1<=B && B<=max)
+	do{
+		srand((unsigned int)time(NULL)); 
+		for(i=0;i<N;i++){
+			for(j=0;j<N;j++){
+				Num = rand()%max+1;
+			}
+		}
+	} while(Num == Table1[i][j]);{
+		process_bingo(N, Num, Table1, Table2);
+	}
 } 
